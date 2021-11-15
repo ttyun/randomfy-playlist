2aac4b46cb957334435ac044ea6c8eb01c7b0346
Tyler Yun <tyler.yun1@gmail.com>
2021-11-14 23:31:00 -0800

    Utilize Flask to handle server requests.

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,34 @@
 import os
-
+import spotipy
+from flask import Flask, request, url_for, session, redirect
 from spotify_client import SpotifyClient
+
+app = Flask(__name__)
+# app.secret_key = 'ASD27DWT312GJKD'
+# app.config['SESSION_COOKIE_NAME'] = 'Ty Cookie Session'
+AUTH_TOKEN_INFO = "auth_token_info"
+
+@app.route('/')
+def login():
+   oauth = SpotifyClient.create_spotify_oauth()
+   auth_url = oauth.get_authorize_url()
+   print(auth_url)
+   return redirect(auth_url)
+
+@app.route('/redirect')
+def redirectApplication():
+   oauth = SpotifyClient.create_spotify_oauth()
+   # session.clear()
+   auth_code = request.args.get('code')
+   auth_token_info = oauth.get_access_token(auth_code)
+   # session[AUTH_TOKEN_INFO] = auth_token_info
+   return redirect(url_for('processPlaylist', _external=True))
+
+@app.route('/playlists')
+def processPlaylist():
+   #generatePlaylist()
+   return 'Some Drake songs'
+
 
 def generatePlaylist():
    playlist_name = 'AUTOPLAY'
@@ -38,4 +66,5 @@ def generatePlaylist():
          print(f"Added track: {track['name']}")
 
 if __name__ == '__main__':
-   generatePlaylist()
+   app.run(port=5000, debug=True)
+   #generatePlaylist()
