92e577de5fb14bd935241d7e0150a22899a91d5a
Tyler Yun <tyler.yun1@gmail.com>
2021-11-18 01:04:12 -0800

    Hooked front end to back end - button click will trigger playist generation.

--- flask-server/app.py
+++ flask-server/app.py
@@ -1,33 +1,43 @@
 import os
 import spotipy
-from flask import Flask, request, url_for, session, redirect
+from flask import Flask, request, url_for, session, redirect, jsonify
+from flask_cors import CORS, cross_origin # Good to know in the future
 from spotify_client import SpotifyClient
 
 app = Flask(__name__)
+CORS(app)
+
+# *** Could add resources to above CORS constructor ***
+# cors_config = {
+#    "origins": ["http://localhost:3000/"],
+#    "methods": ["OPTIONS", "GET"],
+#    "allow_headers": ["Authorization"]
+# }
+
 # app.secret_key = 'ASD27DWT312GJKD'
 # app.config['SESSION_COOKIE_NAME'] = 'Ty Cookie Session'
-AUTH_TOKEN_INFO = "auth_token_info"
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def login():
    oauth = SpotifyClient.create_spotify_oauth()
    auth_url = oauth.get_authorize_url()
    print(auth_url)
    return redirect(auth_url)
 
-@app.route('/redirect')
+@app.route('/redirect', methods=['GET'])
 def redirectApplication():
    oauth = SpotifyClient.create_spotify_oauth()
    # session.clear()
    auth_code = request.args.get('code')
    auth_token_info = oauth.get_access_token(auth_code)
+   print(auth_token_info)
    # session[AUTH_TOKEN_INFO] = auth_token_info
    return redirect(url_for('processPlaylist', _external=True))
 
-@app.route('/playlists')
+@app.route('/playlists', methods=['GET'])
 def processPlaylist():
-   #generatePlaylist()
-   return 'Some Drake songs'
+   added_track_names = generatePlaylist()
+   return jsonify(added_tracks=added_track_names)
 
 
 def generatePlaylist():
@@ -42,10 +52,10 @@ def generatePlaylist():
    spotify_client = SpotifyClient(auth_token)
    
    # Collect random list of top edm songs
-   random_edm_tracks = spotify_client.get_random_tracks(20)
+   random_edm_tracks = spotify_client.get_random_tracks(5)
    
    # Collect random list of top rap songs
-   random_rap_tracks = spotify_client.get_random_tracks(20)
+   random_rap_tracks = spotify_client.get_random_tracks(5)
 
    # Collect random list of album songs
    #random_album_tracks = spotify_client.get_random_tracks_from_album('Whole Lotta Red')
@@ -61,10 +71,12 @@ def generatePlaylist():
    # Add combined tracks into Spotify playlist
    was_added = spotify_client.add_tracks_to_playlist(playlist_track_uris,
          playlist_name, description, isPublic)
+   added_track_names = []
    if was_added:
       for track in playlist_tracks:
          print(f"Added track: {track['name']}")
+         added_track_names.append(track['name'])
+   return added_track_names
 
 if __name__ == '__main__':
-   app.run(port=5000, debug=True)
-   #generatePlaylist()
+   app.run(debug=True, port=5000)
