b8b1f2d4587df957bea1bd00430bf5e8ee8d6ad0
Tyler Yun <tyler.yun1@gmail.com>
2021-11-27 17:28:51 -0800

    Add spotify credentials.

---
+++ react-client/src/SpotifyCredentials.js
@@ -0,0 +1,8 @@
+export const CLIENT_ID = "<CLIENT_ID>";
+export const SPOTIFY_AUTHORIZE_ENDPOINT = "https://accounts.spotify.com/authorize";
+export const REDIRECT_URL_AFTER_LOGIN = "http://localhost:3000/home";
+export const SPACE_DELIMETER = "%20";
+export const SCOPES = [
+   "playlist-modify-public"
+];
+export const SCOPES_URL_PARAM = SCOPES.join(SPACE_DELIMETER);
